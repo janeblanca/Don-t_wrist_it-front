9461bcc81a8503db3d36bb6ff64163db19321a24
Lloyd Aldrin <qlatpornobi@tip.edu.ph>
2024-01-07 21:12:47 +0800

    python hand-detect.py

--- hand_detect.py
+++ hand_detect.py
@@ -1,10 +1,10 @@
 import cv2
-import mediapipe as mp
+import mediapipe as mp_mediapipe
 import numpy as np
 
-mp_hands = mp.solutions.hands
+mp_hands = mp_mediapipe.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
-mp_drawing = mp.solutions.drawing_utils
+mp_drawing = mp_mediapipe.solutions.drawing_utils
 
 def draw_landmarks(frame):
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -20,18 +20,22 @@ def extract_landmarks(frame):
     results = hands.process(frame_rgb)
 
     landmarks_data = []
-    num_hands = min(2, len(results.multi_hand_landmarks))
-    combined_landmarks = []
-    for hand_idx in range(num_hands):
-        landmarks = []  # Initialize landmarks for each hand separately
-        hand_landmarks = results.multi_hand_landmarks[hand_idx]
-        for idx, landmark in enumerate(hand_landmarks.landmark):
-            landmark_data = [landmark.x, landmark.y]
-            if hasattr(landmark, 'z'):
-                landmark_data.append(landmark.z)
-            landmarks.extend(landmark_data)
-        landmarks_data.append(landmarks)
-        combined_landmarks.extend(landmarks)
+    if results.multi_hand_landmarks:
+        num_hands = min(2, len(results.multi_hand_landmarks))
+        combined_landmarks = []
+        for hand_idx in range(num_hands):
+            landmarks = []  # Initialize landmarks for each hand separately
+            hand_landmarks = results.multi_hand_landmarks[hand_idx]
+            for idx, landmark in enumerate(hand_landmarks.landmark):
+                landmark_data = [landmark.x, landmark.y]
+                if hasattr(landmark, 'z'):
+                    landmark_data.append(landmark.z)
+                landmarks.extend(landmark_data)
+            landmarks_data.append(landmarks)
+            combined_landmarks.extend(landmarks)
+    else:
+        # If no hands are detected, return an empty list or handle it as needed
+        combined_landmarks = []
 
     return combined_landmarks
 
@@ -48,7 +52,8 @@ while cap.isOpened():
     landmarks_arr = np.array(landmarks)
     reshaped_landmarks = landmarks_arr.reshape((1, 1, landmarks_arr.shape[0]))
 
-  #   predictions = model.predict(reshaped_landmarks)
+    # Uncomment the following lines when you have the model for predictions
+    # predictions = model.predict(reshaped_landmarks)
 
     print(landmarks_arr)
     print(landmarks_arr.shape)
