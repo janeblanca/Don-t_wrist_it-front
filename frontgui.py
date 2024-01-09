3eb709b4dd16499a60a6c73cf0961a14b3ba6f11
zolatido <119398300+zolatido@users.noreply.github.com>
2024-01-09 14:53:33 +0800

    updated front w/ cam & user input (not final)

--- frontgui.py
+++ frontgui.py
@@ -1,11 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QPainter, QColor, QPen, QFont, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget,  QLineEdit, QLabel, QPushButton
+from PyQt5.QtGui import QPainter, QColor, QPen, QFont, QPixmap, QIntValidator
 from PyQt5.QtCore import Qt, QRect
-
+import cv2
 
 from camera import Camera
-
 
 class MyWindow(QWidget):
     def __init__(self):
@@ -13,6 +12,7 @@ class MyWindow(QWidget):
 
         # Set up the UI components
         self.init_ui()
+
         # Disable maximized window
         self.setWindowFlags(self.windowFlags() & ~Qt.WindowMaximizeButtonHint)
 
@@ -20,11 +20,23 @@ class MyWindow(QWidget):
         self.camera = Camera(self)
         self.timer = self.startTimer(1)
 
+        #
+        self.user_input = QLineEdit(self)
+        self.user_input.setGeometry(355, 195, 50, 40)
+        self.user_input.setStyleSheet( "background-color: #f3f1ec; border: none; color: #303030; font-size: 30px;")
+
+        # accept integers only
+        int_validator = QIntValidator()
+        self.user_input.setValidator(int_validator)
+
+
+        self.user_input.returnPressed.connect(self.set_break_time)
+        self.break_time = 0
+
     def init_ui(self):
         self.setWindowTitle("Don't Wrist It")
-        self.setGeometry(350, 200, 1280, 720)
         self.setStyleSheet("background-color: #f3f1ec;")
-        self.setFixedSize(1280, 720) # fixed size
+        self.setFixedSize(1200, 720)  # fixed size
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -41,45 +53,95 @@ class MyWindow(QWidget):
 
         # AUDIO
         painter.setBrush(QColor(74, 73, 73, int(0.23 * 255)))
-        audio_pane = QRect(105, self.height() - 125, self.width() - 577, 85)
+        audio_pane = QRect(105, self.height() - 115, self.width() - 577, 85)
         radius = 13  # Set the radius for rounded corners
         painter.drawRoundedRect(audio_pane, radius, radius)
 
         # small square (box of audio icon)
-        painter.setBrush(QColor("#CF6B6E"))
-        square_audio_box = QRect(123, self.height() - 110, 50, 50)
+        painter.setBrush(QColor("#FBF0F3"))
+        square_audio_box = QRect(123, self.height() - 97, 50, 50)
         radius = 8  # Set the radius for rounded corners
         painter.drawRoundedRect(square_audio_box, radius, radius)
 
         # WORKTIME
         painter.setBrush(QColor("#FFFFFF"))
-        worktime= QRect(105, self.height() - 600, 200, 200)
+        # worktime = QRect(105, self.height() - 600, self.width() - 1000, self.height() - 520)
+        worktime= QRect(105, 115, 200, 230)
         radius = 8  # Set the radius for rounded corners
         painter.drawRoundedRect(worktime, radius, radius)
 
+        # small square ICON (Green Clock)
+        painter.setBrush(QColor("#D0FFCF"))
+        square_green_box= QRect(119, self.height() - 593, 50, 50)
+        radius = 8  # Set the radius for rounded corners
+        painter.drawRoundedRect(square_green_box, radius, radius)
+
+        # small square ICON (Worktime)
+        painter.setBrush(QColor("#D0FFCF"))
+        square_green_box = QRect(118, self.height() - 475,105, 30)
+        radius = 8  # Set the radius for rounded corners
+        painter.drawRoundedRect(square_green_box, radius, radius)
+
         # BREAKTIME
         painter.setBrush(QColor("#FFFFFF"))
-        breaktime = QRect(357, self.height() - 600, 200, 200)
+        breaktime = QRect(317, 115, 200, 230)
         radius = 8  # Set the radius for rounded corners
         painter.drawRoundedRect(breaktime, radius, radius)
 
+        # small square ICON (BLue Clock)
+        painter.setBrush(QColor("#D0FBFF"))
+        square_blue_box = QRect(329, self.height() - 593, 50, 50)
+        radius = 8  # Set the radius for rounded corners
+        painter.drawRoundedRect(square_blue_box, radius, radius)
+
+        # small square ICON (Breaktime)
+        painter.setBrush(QColor("#D0FBFF"))
+        square_blue_box = QRect(328, self.height() - 475, 105, 30)
+        radius = 8  # Set the radius for rounded corners
+        painter.drawRoundedRect(square_blue_box, radius, radius)
+
         # BREAk INTERVAL
         painter.setBrush(QColor("#FFFFFF"))
-        breakinterval = QRect(608 , self.height() - 600, 200, 200)
+        breakinterval = QRect(529, 115, 200, 230)
         radius = 8  # Set the radius for rounded corners
         painter.drawRoundedRect(breakinterval, radius, radius)
 
+        # small square ICON (Yellow Clock)
+        painter.setBrush(QColor("#FFF7AE"))
+        square_yellow_box = QRect(540, self.height() - 593, 50, 50)
+        radius = 8  # Set the radius for rounded corners
+        painter.drawRoundedRect(square_yellow_box, radius, radius)
+
+        # small square ICON (Break Interval)
+        painter.setBrush(QColor("#FFF7AE"))
+        square_yellow_box = QRect(539, self.height() - 475, 105, 30)
+        radius = 8  # Set the radius for rounded corners
+        painter.drawRoundedRect(square_yellow_box, radius, radius)
+
         # WRIST POSITION
         painter.setBrush(QColor("#FFFFFF"))
-        worktime = QRect(105, self.height() - 360, 200, 200)
+        worktime = QRect(105, 360, 200, 230)
         radius = 8  # Set the radius for rounded corners
         painter.drawRoundedRect(worktime, radius, radius)
 
         # Reminder
         painter.setBrush(QColor("#FFFFFF"))
-        reminder = QRect(357, self.height() - 360, self.width() - 828, 200)
+        reminder = QRect(317, 360, self.width() - 788, 230)
         radius = 8  # Set the radius for rounded corners
         painter.drawRoundedRect(reminder, radius, radius)
+
+        # small square ICON (Wrist Position)
+        painter.setBrush(QColor("#D0FBFF"))
+        square_blue_box = QRect(119, self.height() - 347, 50, 50)
+        radius = 8  # Set the radius for rounded corners
+        painter.drawRoundedRect(square_blue_box, radius, radius)
+
+        # small square ICON (Correct - Wrist Position)
+        painter.setBrush(QColor("#D0FBFF"))
+        square_blue_box = QRect(118, 496, 105, 30)
+        radius = 8  # Set the radius for rounded corners
+        painter.drawRoundedRect(square_blue_box, radius, radius)
+
 
         # ------------TEXTS----------
         # Don't Wrist It (Dashboard)
@@ -101,7 +163,7 @@ class MyWindow(QWidget):
         font_title.setPointSize(9)
         painter.setFont(font_title)
         painter.setPen(QColor("#303030"))
-        painter.drawText(200, self.height()-95, 450, 270, Qt.AlignLeft, "Prolonged incorrect wrist position! Correct your position immediately.")
+        painter.drawText(200, self.height()-85, 450, 270, Qt.AlignLeft, "Prolonged incorrect wrist position! Correct your position immediately.")
 
         # Don't Wrist It (Logo)
         font_title2 = QFont()
@@ -110,29 +172,73 @@ class MyWindow(QWidget):
         painter.setPen(QColor("#ffffff"))
         painter.drawText(self.width() - 450, 100, 450, 270, Qt.AlignCenter, "DON'T WRIST IT")
 
-        painter.setPen(QColor("#4F4E4E"))
-        painter.drawLine(self.width() - 420, 275, self.width() - 43, 275)
-
         # Setting-up Camera
         font_desc = QFont()
         font_desc.setPointSize(10)
         painter.setFont(font_desc)
-        painter.setPen(QColor("#ffffff"))
-        painter.drawText(self.width() - 420, 295, 450, self.height(), Qt.AlignLeft, "Setting-up your Camera")
-
-       # Camera
-        self.camera.cam_container(painter)
-        if self.camera.cam_placeholder:
-            self.camera.cam_holder(painter)
-        else:
-            self.camera.cam_draw(painter)
+        painter.drawText(self.width() - 430, 305, 450, self.height(), Qt.AlignLeft, "Setting-up your Camera")
 
         #Worktime
         font_title = QFont()
         font_title.setPointSize(9)
         painter.setFont(font_title)
         painter.setPen(QColor("#303030"))
-        painter.drawText(150, 260, 450, 270, Qt.AlignLeft, "Work Time")
+        painter.drawText(134, 250, 450, 270, Qt.AlignLeft, "Work Time")
+
+        # Breaktime
+        font_title = QFont()
+        font_title.setPointSize(9)
+        painter.setFont(font_title)
+        painter.setPen(QColor("#303030"))
+        painter.drawText(344, 250, 450, 270, Qt.AlignLeft, "Break Time")
+
+        font_title = QFont()
+        font_title.setPointSize(7)
+        painter.setFont(font_title)
+        painter.setPen(QColor("#282828"))
+        painter.drawText(420, 205, 400, 270, Qt.AlignLeft, "mins")
+
+        # Breakinterval
+        font_title = QFont()
+        font_title.setPointSize(9)
+        painter.setFont(font_title)
+        painter.setPen(QColor("#303030"))
+        painter.drawText(545, 250, 450, 270, Qt.AlignLeft, "Break Interval")
+
+        font_title = QFont()
+        font_title.setPointSize(7)
+        painter.setFont(font_title)
+        painter.setPen(QColor("#282828"))
+        painter.drawText(630, 205, 400, 270, Qt.AlignLeft, "mins")
+
+        # Wrist Position
+        font_title = QFont()
+        font_title.setPointSize(9)
+        painter.setFont(font_title)
+        painter.setPen(QColor("#303030"))
+        painter.drawText(190, 390, 450, 270, Qt.AlignLeft, "Wrist Position")
+
+        # Correct (Wrist Position)
+        font_title = QFont()
+        font_title.setPointSize(9)
+        painter.setFont(font_title)
+        painter.setPen(QColor("#303030"))
+        painter.drawText(149, 501, 450, 270, Qt.AlignLeft, "Correct")
+
+        # Reminder
+        font_title = QFont()
+        font_title.setPointSize(9)
+        painter.setFont(font_title)
+        painter.setPen(QColor("#303030"))
+        painter.drawText(335, 390, 450, 270, Qt.AlignLeft, "Reminder")
+
+        # Camera
+        self.camera.cam_container(painter)
+        if self.camera.cam_placeholder:
+            self.camera.cam_holder(painter)
+        else:
+            self.camera.cam_draw(painter)
+
 
         #------------IMAGES----------
         # image LOGO
@@ -142,7 +248,23 @@ class MyWindow(QWidget):
 
         # Image AUDIO
         image_audio = QPixmap("./src/audio_icon.png")
-        painter.drawPixmap(136, self.height()-98, 27, 27,image_audio)
+        painter.drawPixmap(133, self.height()- 88, 32,30,image_audio)
+
+        # Image GREEN clock
+        image_green = QPixmap("./src/green_clock.png")
+        painter.drawPixmap(80, self.height() - 605, 130, 75, image_green)
+
+        # Image BLUE clock
+        image_blue = QPixmap("./src/blue_clock.png")
+        painter.drawPixmap(290, self.height() - 605, 130, 75, image_blue)
+
+        # Image YELLOW clock
+        image_yellow = QPixmap("./src/yellow_clock.png")
+        painter.drawPixmap(500, self.height() - 605, 130, 75, image_yellow)
+
+        # Image Wrist
+        image_yellow = QPixmap("./src/wrist.png")
+        painter.drawPixmap(45, 340, 200, 115, image_yellow)
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -161,6 +283,16 @@ class MyWindow(QWidget):
     def closeEvent(self, event):
         self.camera.release_camera()
         event.accept()
+
+    def set_break_time(self):
+        # Get the break time duration
+        break_time_str = self.user_input.text()
+        try:
+            self.break_time = int(break_time_str)
+            print(f"Break time set to {self.break_time} minutes.")
+        except ValueError:
+            print("Invalid input. Please enter a valid integer for break time.")
+
 
 
 if __name__ == '__main__':
